0fa6e1fcd880de1206c7792559ab84921a6b34da
Daniel Padilla <danielpadilla@Daniels-MacBook-Pro.local>
2017-05-18 18:40:51 -0500

    add platform

--- #SERVER_SIDE/food_menu.php
+++ #SERVER_SIDE/food_menu.php
@@ -3,7 +3,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-$conn = new mysqli("localhost", "user", "password", "db");
+$conn = new mysqli("localhost", "groupwls_daniel", "IJxC&{cruv]e", "groupwls_tienda");
 
 	$till=10;
 	
